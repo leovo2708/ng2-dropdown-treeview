42182bcd6be796ad4c7c6fce516a72aa435a5947
Leo Vo <leo.vo@nexcel.vn>
2017-03-29 12:41:43 +0700

    Update demo.

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -180,8 +180,7 @@ export class AppComponent {
         for (let i = 0; i < 1000; i++) {
             const value: any = i === 0 ? { name: `${i}` } : i;
             const checked = i % 100 === 0;
-            const item = new TreeviewItem({ text: `Item ${i}`, value: value });
-            item.checked = checked;
+            const item = new TreeviewItem({ text: `Item ${i}`, value: value, checked: checked });
             items.push(item);
         };
         return items;
